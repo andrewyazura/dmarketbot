9024686a03591eef8aadb6a232a136ac68b4f5f3
andrew4ever <andrewyazura203@gmail.com>
2020-06-16 20:56:33 +0300

    Implement ItemFilter and add config.yaml

--- App/ItemFilter/ItemFilter.py
+++ App/ItemFilter/ItemFilter.py
@@ -1,20 +1,37 @@
 import logging
 
+import yaml
 from tinydb import Query, TinyDB
 
 logger = logging.getLogger(__name__)
+Item = Query()
 
 
 class ItemFilter:
-    def __init__(self):
+    def __init__(self, config):
         self.db = TinyDB('data/db.json')
         self.items_table = self.db.table('all_items')
         self.best_items_table = self.db.table('best_items')
+        self.config = config
+
+        logger.debug('__init__() finished')
 
     def filter(self):
-        pass
+        logger.debug('filter() started')
+
+        l = self.items_table.search(
+            (Item.exterior.one_of(self.config['exteriors'])) &
+            (Item.profit >= self.config['min_profit'])
+        )
+
+        for item in l:
+            self.best_items_table.insert(item)
+
+        logger.info('%i items are considered good', len(l))
 
     def quit(self):
+        logger.debug('quit() started')
+
         self.db.close()
 
 
@@ -22,6 +39,9 @@ if __name__ == '__main__':
     logging.basicConfig(filename='itemfilter.log',
                         filemode='w', level=logging.INFO)
 
-    f = ItemFilter()
+    with open('config.yaml', 'r') as conf:
+        config = yaml.safe_load(conf)
+
+    f = ItemFilter(config)
     f.filter()
     f.quit()
--- config.py
+++
@@ -1,3 +0,0 @@
-config = {
-    'max_price': 500
-}
