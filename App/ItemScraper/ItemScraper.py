d1857b7374c4e1489faa0183b123e4d49a3471c7
andrew4ever <andrewyazura203@gmail.com>
2020-06-16 12:18:01 +0300

    ItemScraper gets all data about item, including URL

--- App/ItemScraper/ItemScraper.py
+++ App/ItemScraper/ItemScraper.py
@@ -33,12 +33,10 @@ class ItemScraper:
         for card in self.cards:
             self.__open_card(card)
 
-            self.get_item_information(card)
+            info = self.__get_item_information()
+            print(info)
 
             self.__close_overlapping_menu()
-
-    def get_item_information(self, card):
-        print(card)
 
     def __open_card(self, card):
         try:
@@ -49,6 +47,62 @@ class ItemScraper:
         except:
             self.__dismiss_sidebar()
             self.__open_card(card)
+
+    def __get_item_information(self):
+        overlay = self.driver.find_element_by_class_name(
+            'cdk-global-overlay-wrapper')
+
+        item_info_blocks = overlay.find_elements_by_class_name(
+            'c-assetPreview__inner')
+
+        url = self.__get_url(item_info_blocks[0])
+        data = self.__get_data(item_info_blocks[1])
+
+        return {**data, 'url': url}
+
+    def __get_url(self, block):
+        button = block.find_element_by_css_selector(
+            'button.c-shareLink__btn.mat-flat-button')
+        button.click()
+
+        div = block.find_element_by_class_name('mat-form-field-infix')
+        input_block = div.find_element_by_tag_name('input')
+        url = input_block.get_attribute("value")
+
+        return url
+
+    def __get_data(self, block):
+        title = block.find_element_by_tag_name('h3').text.strip()
+        exterior = self.__get_weapon_exterior(title)
+
+        prices = [float(span.text.strip())
+                  for span in
+                  block.find_elements_by_class_name(
+            'c-assetPreviewParam__number')]
+
+        profit = round(abs(prices[1] - prices[0]), 2)
+        discount = block.find_element_by_tag_name(
+            'asset-discount-badge').text
+
+        trade_lock = block.find_element_by_css_selector(
+            'p.c-assetPreviewParam__value').text.strip()
+
+        data = {
+            'title': title,
+            'exterior': exterior,
+            'trade_lock': trade_lock,
+            'dmarket_price': prices[0],
+            'steam_price': prices[1],
+            'profit': profit,
+            'discount': discount
+        }
+
+        return data
+
+    def __get_weapon_exterior(self, title):
+        exterior = title.split('(')[-1]
+
+        return exterior[:-1]
 
     def __close_overlapping_menu(self):
         try:
@@ -61,17 +115,6 @@ class ItemScraper:
             self.__dismiss_notifications()
             self.__close_overlapping_menu()
 
-    def __dismiss_sidebar(self):
-        try:
-            sidebar = self.driver.find_element_by_class_name('с-seoArea')
-            sidebar.find_element_by_class_name(
-                'с-seoArea__headerClose').click()
-
-        except:
-            print('couldn\'t close sidebar')
-            # self.__dismiss_notifications()
-            # self.__dismiss_sidebar()
-
     def __dismiss_notifications(self):
         try:
             self.wait.until(EC.visibility_of_element_located(
@@ -81,6 +124,15 @@ class ItemScraper:
 
         except:
             print('notification didn\'t appear')
+
+    def __dismiss_sidebar(self):
+        try:
+            sidebar = self.driver.find_element_by_class_name('с-seoArea')
+            sidebar.find_element_by_class_name(
+                'с-seoArea__headerClose').click()
+
+        except:
+            print('couldn\'t close sidebar')
 
 
 scraper = ItemScraper(
